3ebbec0a1127d5f92da87811c84445fbb0551dfb
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-30 04:59:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,8 @@ end
 def batch_badge_creator(arr)
   arr2 = []
   arr.each do |peeps|
-    arr2.push(peeps)
-    return "Hello, my name is #{peeps}."
+    arr2.push("Hello, my name is #{peeps}")
+    return 
     
   end
   
